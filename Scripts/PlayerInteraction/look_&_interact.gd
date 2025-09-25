6073c6dc0abe0d1549c4ba8ecc111a1c931302d5
Alex Flores <71346583+Ar3kka@users.noreply.github.com>
2025-09-25 15:02:14 -0600

    Small hotfix and enhancing look & interact script
    Fixed : Now you're able to drag objects even if they're not selectable, and can only drag objects that are draggable, even if they're not selectable.
    
    Introduced :
    - (H) to turn on and off holding drag
    - (G) to recover selected piece horizontally to mouse
    
    (Horizontal and vertical are still switched up and I'm lazy to fix it lul)

--- Scripts/PlayerInteraction/look_&_interact.gd
+++ Scripts/PlayerInteraction/look_&_interact.gd
@@ -2,8 +2,9 @@ extends Node3D
 
 @export var camera : Camera3D
 @export var team : int = 0
-@export var manual_rotation_strength : float = 3.5
+@export var holding_drag : bool = true
 @export var drag_strength : float = 0.1
+@export var manual_rotation_strength : float = 3.5
 var raycast : RayCast3D
 @export var raycast_distance : float = 250.0
 var mouse_position : Vector2
@@ -26,6 +27,7 @@ func _ready() -> void:
 	add_child(raycast)
 
 func stop_dragging(draggable_component : DraggableComponent) :
+	if !draggable_component : return
 	draggable_component.stop_dragging()
 	if !dragging(): return
 	horizontal_drag = false
@@ -48,6 +50,9 @@ func _process(_delta):
 	raycast.target_position = camera.project_local_ray_normal(mouse_position) * raycast_distance
 	raycast.force_raycast_update()
 	
+	###### SWITCH DRAGGING TYPE
+	if Input.is_action_just_pressed("Switch Drag Mode"): holding_drag = !holding_drag
+	
 	###### DETECT LOOKABLE, SELECTABLE AND DRAGGABLE OBJECT
 	var looked_at_nothing : bool = true
 	if !dragging(): detected_object = raycast.get_collider()
@@ -68,14 +73,13 @@ func _process(_delta):
 			# let the lookable object know that it's not being looked at anymore and reset object variables
 			lookable_object.lookable_component.look.emit(false)
 			detected_object = null
-			lookable_object = null
 			selectable_object = null
+			draggable_object = null
 
 	# if the object IS NOT selectable and there's not an object already selected, skip
-	if selectable_object == null && selected_object == null: return
+	if selectable_object == null && selected_object == null || lookable_object == null: return
 	
 	###### Checking selection inputs
-	var draggable_component : DraggableComponent 
 	if selectable_object:
 		if Input.is_action_just_pressed("Select"):
 			if selectable_object != selected_object: _deselect()
@@ -83,9 +87,22 @@ func _process(_delta):
 			selected_object = selectable_object
 		
 		# Checking if the selectable object IS draggable
-		draggable_component = selectable_object.draggable_component
-		if draggable_component && draggable_component.active: draggable_object = selectable_object
+		#draggable_component = selectable_object.draggable_component
+		#if draggable_component && draggable_component.active: draggable_object = selectable_object
 	else: if looked_at_nothing && Input.is_action_just_pressed("Select") && selected_object: _deselect()
+	
+	# Checking if the object being looked at or selected IS draggable
+	var draggable_component : DraggableComponent
+	var final_draggable_object : Piece
+	if !selected_object: 
+		draggable_component = lookable_object.draggable_component
+		final_draggable_object = lookable_object
+	else: 
+		draggable_component = selected_object.draggable_component
+		final_draggable_object = selected_object
+	
+	if draggable_component && draggable_component.active: draggable_object = final_draggable_object
+	#if draggable_component && draggable_component.active: draggable_object = selectable_object
 	
 	if Input.is_action_just_pressed("Deselect"): return _deselect()
 	
@@ -109,19 +126,22 @@ func _process(_delta):
 		if Input.is_action_just_released("Rotate Left") || Input.is_action_just_released("Rotate Right"): rotatable_component.stop_rotating()
 	
 	####### Checking drag inputs
-	if Input.is_action_just_pressed("Drag Vertical"): vertical_drag = true
-	if Input.is_action_just_pressed("Drag Horizontal"): horizontal_drag = true
-	
-
-	
-	# if draggable object is being dragged continue, if not, skip
-	if !dragging(): return 
+	if Input.is_action_just_pressed("Drag Vertical"):
+		if holding_drag || (!holding_drag && !vertical_drag) : vertical_drag = true
+		else: vertical_drag = false
+	if Input.is_action_just_pressed("Drag Horizontal"): 
+		if holding_drag || (!holding_drag && !horizontal_drag) : horizontal_drag = true
+		else: horizontal_drag = false
 	
 	####### Checking undragging inputs
 	# checking if the object is still being dragged
-	#if (Input.is_action_just_released("Drag Vertical") || Input.is_action_just_released("Drag Horizontal")) && !double_drag():
-	if !Input.is_action_pressed("Drag Vertical"): vertical_drag = false
-	if !Input.is_action_pressed("Drag Horizontal"): horizontal_drag = false
+	if holding_drag:
+		if !Input.is_action_pressed("Drag Vertical"): vertical_drag = false
+		if !Input.is_action_pressed("Drag Horizontal"): horizontal_drag = false
+	
+	####### RECOVER DRAG OBJECT INPUTS
+	if Input.is_action_pressed("Recover") && !dragging(): vertical_drag = true
+	
 	if !dragging(): return stop_dragging(draggable_component) # If no dragging action is recorded, we stop dragging.
 	
 	####### Calculating the position where the SELECTED object needs to move at
