d0c2312891728ab9fa0ad46ab2a57d375e8afb3b
Alex Flores <71346583+Ar3kka@users.noreply.github.com>
2025-09-30 00:06:33 -0600

    Tile grid color pattern integration, and tile translation functions and signal
    Added a function for the tile grid to apply a color pattern to the generated grid.
    
    Added functions to translate and move tiles to the tiles themselves

--- Scripts/TileComponents/tile.gd
+++ Scripts/TileComponents/tile.gd
@@ -1,23 +1,44 @@
 class_name Tile extends Node3D
 
-const SCENE: PackedScene = preload("res://Scenes/Tiles/tile.tscn")
+signal translate(translation_vector : Vector3)
 
+signal raise(height : float)
+
+const SCENE : PackedScene = preload("res://Scenes/Tiles/tile.tscn")
+const STANDARD_UP_DOWN_DISTANCE : float = 0.250
+const STANDARD_LEFT_RIGHT_DISTANCE : float = 1.0
+const STANDARD_TRANSLATION_STRENGTH : float = 0.05
+
+var id : Vector2i = Vector2i.ZERO
+## The meshinstance3D set for this tile.
 @export var appearance : MeshInstance3D
 var skin : StandardMaterial3D
+## The color for the tile surface material.
 @export var tint : Color = Color.WHITE :
 	set(new_tint): 
 		tint = new_tint
 		set_color(tint)
-
+## If not active it will not work in any way.
 @export var active : bool = true :
 	set(new_active_state):
 		active = new_active_state
 		visible = new_active_state
-		 
+## Whether or not this tile can have a piece snapped to its snap area.
 @export var snappable : bool = true
+## Whether or not this tile is movable through its move and translation functions
+@export var movable : bool = true
 var occupier : Piece
 var _snap_area : Area3D
-var id : Vector2i = Vector2i.ZERO 
+
+var _translation_vector : Vector3 = Vector3.ZERO :
+	set(new_vector) :
+		if new_vector == Vector3.ZERO :
+			_translation_vector = new_vector
+			return
+		new_vector.x += global_position.x
+		new_vector.y += global_position.y
+		new_vector.z += global_position.z
+		_translation_vector = new_vector
 
 func instantiate(new_active_state : bool = true, new_snappable_state : bool = true) -> Tile:
 	var new_tile : Tile = SCENE.instantiate()
@@ -26,15 +47,19 @@ func instantiate(new_active_state : bool = true, new_snappable_state : bool = tr
 	return new_tile
 
 func _ready() -> void:
-	set_color(tint)
+	translate.connect(translation)
 	appearance = %Mesh
 	_snap_area = %Snap
+	set_color(tint)
+	
 	if !_snap_area : return
+	
 	_snap_area.area_entered.connect(func (area : Area3D):
 		var parent = area.get_parent_node_3d()
 		#if parent is Hands : return
 		if parent is SnappableComponent: 
 			occupier = parent.body)
+	
 	_snap_area.area_exited.connect(func (area : Area3D):
 		var parent = area.get_parent_node_3d()
 		if parent is Hands :
@@ -51,3 +76,38 @@ func set_color(new_tint : Color):
 	skin = StandardMaterial3D.new()
 	skin.albedo_color = new_tint
 	appearance.set_surface_override_material(0, skin)
+
+func translation(translation_direction : Vector3 = Vector3.ZERO):
+	_translation_vector = translation_direction
+	
+func move_up(times : int = 1): _translation_vector = Vector3(0, STANDARD_UP_DOWN_DISTANCE * times, 0)
+
+func move_down(times : int = 1): _translation_vector = Vector3(0, -STANDARD_UP_DOWN_DISTANCE * times, 0)
+
+func move_left(times : int = 1): _translation_vector = Vector3(-STANDARD_LEFT_RIGHT_DISTANCE * times, 0, 0)
+
+func move_right(times : int = 1): _translation_vector = Vector3(STANDARD_LEFT_RIGHT_DISTANCE * times, 0, 0)
+
+func move_forth(times : int = 1): _translation_vector = Vector3(0, 0, STANDARD_LEFT_RIGHT_DISTANCE * times)
+
+func move_back(times : int = 1): _translation_vector = Vector3(0, 0, -STANDARD_LEFT_RIGHT_DISTANCE * times)
+
+func stop_translation():
+	_translation_vector = Vector3.ZERO
+
+func is_translating() -> bool : return _translation_vector != Vector3.ZERO
+
+func _has_translation_reached_goal() -> bool :
+	return _round_to_decimal(global_position) == _round_to_decimal(_translation_vector)
+
+func _round_to_decimal(num, digit : int = 2):
+	return round(num * pow(10.0, digit)) / pow(10.0, digit)
+	
+func _process(delta: float) -> void:
+	if !active || !movable: return
+	
+	if _has_translation_reached_goal() : stop_translation()
+	
+	if !is_translating() : return
+	
+	global_position = lerp(global_position, _translation_vector, STANDARD_TRANSLATION_STRENGTH)
--- Scripts/TileComponents/tile_map.gd
+++ Scripts/TileComponents/tile_map.gd
@@ -2,6 +2,8 @@ class_name TileGrid extends Node3D
 
 const STANDARD_SIZE : int = 8
 const STANDARD_DIRECTION : Vector2i = Vector2i(-1, -1)
+const STANDARD_COLOR_PATTERN : Array[Color] = [Color.WHITE, Color.BLACK]
+const STANDARD_RAISE_HEIGHT : float = 0.250
 
 @export var active : bool = true
 ## The tiles that this tile grid can generate randomly throughout the map generation accounting by their
@@ -22,34 +24,69 @@ const STANDARD_DIRECTION : Vector2i = Vector2i(-1, -1)
 		if new_direction.y > 1 : generation_direction.y = 1
 		if new_direction.x < -1 : generation_direction.x = -1
 		if new_direction.y < -1 : generation_direction.y = -1
+## Forces the tint of the tiles to be overwritten by the color pattern entered below.
+@export var force_pattern : bool = true
+## The color pattern that the generation will follow with each printed tile.
+## (From 0 onward)
+@export var color_pattern : Array[Color] = STANDARD_COLOR_PATTERN
+## Switch the color palette every other row in case the color palette matches the grid size
+## causing it to merge, turn on or off this variable to change when to skip a color every other row.
+@export var color_switch : bool = true
 var tiles : Array[Array] 
 
 func _ready() -> void:
 	if !tile_types.size(): _reset_tyle_types()
 	generate_tiles()
+	get_tile_at().move_back()
 
 func _reset_tyle_types():
 	if !active : return
 	tile_types = []
 	tile_types.append(Tile.new())
 
-func get_tile_at(horizontal_coordinate : int, vertical_coordinate : int) -> Tile:
-	return tiles.get(horizontal_coordinate).get(vertical_coordinate)
+func get_tile_at(tile_coordinates : Vector2 = Vector2.ZERO) -> Tile:
+	return tiles.get(tile_coordinates.x).get(tile_coordinates.y)
 	
 func generate_tiles():
 	if !active : return
-	var black : bool = false
+	
+	# Check and initialize the final size of the desired grid
 	var final_size : Vector2i = grid_size
 	if randomize : final_size = Vector2i(randi_range(grid_size.x, grid_size.y), randi_range(grid_size.x, grid_size.y)) 
+	# Initialize and calculate the color index in case needed
+	# Boolean to check if the color pattern is even and switch accordingly
+	var switch_pattern : bool = color_switch
+	var color_index = 0
+	if color_pattern.size() < 1 : force_pattern = false
+	
+	## Vertical Tiles
 	for z in range(final_size.y):
+		# Check if the color patterns are even or odd.
+		if color_switch:
+			switch_pattern = !switch_pattern
+			if switch_pattern && color_index + 1 < color_pattern.size() : color_index += 1
+			else: color_index = 0
+		# Initialize the new row of vertical tiles.
 		var tile_array : Array = []
-		if final_size.y % 2 == 0: black = !black
+		## Horizontal Tiles
 		for x in range(final_size.x):
+			# Instantiate the new tile and set its position and id.
 			var new_tile = Tile.new().instantiate()
 			new_tile.global_position = Vector3(global_position.x + x * generation_direction.x, global_position.y, global_position.z + z * generation_direction.y)
 			new_tile.id = Vector2i(z, x)
-			if black : new_tile.tint = Color.BLACK
+			
+			# Check color replacement to follow given pattern
+			if force_pattern : new_tile.tint = color_pattern[color_index]
+			if color_index + 1 < color_pattern.size() : color_index += 1
+			else: color_index = 0
+			
+			# Append to array and add child to scene
 			tile_array.append(new_tile)
 			%Tiles.add_child(new_tile)
-			black = !black
+		# After finishing the array append it to the list of vertical arrays
 		if tile_array.size() != 0: tiles.append(tile_array)
+	
+
+func raise_tile(tile_coordinates : Vector2i = Vector2i.ZERO, height : float = STANDARD_RAISE_HEIGHT):
+	var tile_to_raise : Tile = get_tile_at(tile_coordinates)
+	tile_to_raise.translate.emit(Vector3(0, STANDARD_RAISE_HEIGHT, 0))
